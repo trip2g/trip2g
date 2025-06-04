7d0e8af878abd1eee79d2e5b0d83f8fc0a6c0c3e
Alexey Yurchenko <alexes.dev@gmail.com>
2025-06-04 08:41:37 +0000

    feat: add locales to the embed assets, rm unused index.html, update deploy logic

--- assets/embed.go
+++ assets/embed.go
@@ -5,5 +5,5 @@ package assets
 
 import "embed"
 
-//go:embed output.css turbo.js ui/admin/-/web.js ui/user/-/web.js
+//go:embed output.css turbo.js ui/admin/-/web.js ui/user/-/web.js ui/user/-/web.locale* ui/admin/-/web.locale*
 var FS embed.FS
