d1d912d8108cb9b6de3b6a11106e4cf948d6c527
Alexey Yurchenko <alexes.dev@gmail.com>
2025-06-27 05:54:08 +0000

    feat(internal/case/signinbytgauthtoken): add main logic

--- internal/case/signinbytgauthtoken/resolve.go
+++ internal/case/signinbytgauthtoken/resolve.go
@@ -2,7 +2,10 @@ package signinbytgauthtoken
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"trip2g/internal/db"
 	"trip2g/internal/logger"
 	"trip2g/internal/model"
 )
@@ -10,18 +13,48 @@ import (
 type Env interface {
 	Logger() logger.Logger
 	SetupUserToken(ctx context.Context, userID int64) (string, error)
+	TgUserProfileByChatIDAndBotID(ctx context.Context, arg db.TgUserProfileByChatIDAndBotIDParams) (db.TgUserProfile, error)
+	InsertUserWithTgUserID(ctx context.Context, tgUserID sql.NullInt64) (db.User, error)
+	UserByTgUserID(ctx context.Context, tgUserID sql.NullInt64) (db.User, error)
 	ParseTgAuthToken(_ context.Context, token string) (*model.TgAuthToken, error)
 }
 
+var ErrProfileNotFound = errors.New("profile not found")
+
 func Resolve(ctx context.Context, env Env, rawToken string) error {
-	_, err := env.ParseTgAuthToken(ctx, rawToken)
+	token, err := env.ParseTgAuthToken(ctx, rawToken)
 	if err != nil {
 		return fmt.Errorf("failed to parse token: %w", err)
 	}
 
-	// TODO: make user if needed
+	profileParams := db.TgUserProfileByChatIDAndBotIDParams(*token)
 
-	_, err = env.SetupUserToken(ctx, 0)
+	// check if profile exists
+	_, err = env.TgUserProfileByChatIDAndBotID(ctx, profileParams)
+	if err != nil {
+		if db.IsNoFound(err) {
+			return ErrProfileNotFound
+		}
+
+		return fmt.Errorf("failed to get profile by chat ID and bot ID: %w", err)
+	}
+
+	sqlID := sql.NullInt64{Valid: true, Int64: token.ChatID}
+
+	// select or create user by chat id
+	user, err := env.UserByTgUserID(ctx, sqlID)
+	if err != nil {
+		if db.IsNoFound(err) {
+			user, err = env.InsertUserWithTgUserID(ctx, sqlID)
+			if err != nil {
+				return fmt.Errorf("failed to insert user with TG user ID: %w", err)
+			}
+		}
+
+		return fmt.Errorf("failed to get user by TG user ID: %w", err)
+	}
+
+	_, err = env.SetupUserToken(ctx, user.ID)
 	if err != nil {
 		return fmt.Errorf("failed to setup user token: %w", err)
 	}
