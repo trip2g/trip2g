defce9b249d3a82c0474955deeda8850d9e34c9a
Alexey Yurchenko <alexes.dev@gmail.com>
2025-08-23 03:09:02 +0000

    fix(assets): embed fonts

--- assets/embed.go
+++ assets/embed.go
@@ -5,5 +5,5 @@ package assets
 
 import "embed"
 
-//go:embed output.css turbo.js ui/admin/-/web.js ui/user/-/web.js ui/user/-/web.locale* ui/admin/-/web.locale*
+//go:embed output.css turbo.js ui/admin/-/web.js ui/user/-/web.js ui/user/-/web.locale* ui/admin/-/web.locale* *.woff *.woff2
 var FS embed.FS
