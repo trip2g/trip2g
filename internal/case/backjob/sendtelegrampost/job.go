ca1a8eb07b04f854d4dba1b5a3188897f00986cd
Alexey Yurchenko <alexes.dev@gmail.com>
2025-11-05 05:37:30 +0000

    fix(internal/case/backjobs): rm unused interfaces

--- internal/case/backjob/sendtelegrampost/job.go
+++ internal/case/backjob/sendtelegrampost/job.go
@@ -3,20 +3,12 @@ package sendtelegrampost
 import (
 	"context"
 	"trip2g/internal/jobs"
-	"trip2g/internal/logger"
 	"trip2g/internal/model"
 )
 
 const JobID = "send_message"
 const QueueID = model.BackgroundTelegramAPICallQueue
 const Priority = 1 // shoud process before updates
-
-type SendTelegramPostEnv interface {
-	Env
-	Logger() logger.Logger
-	RegisterJob(qID model.BackgroundQueueID, id string, handler func(ctx context.Context, m []byte) error)
-	EnqueueJob(ctx context.Context, job model.BackgroundTask) error
-}
 
 type SendTelegramPostJob struct {
 	enqueue jobs.EnqueueFunc
--- internal/case/backjob/updatetelegrampost/job.go
+++ internal/case/backjob/updatetelegrampost/job.go
@@ -3,20 +3,12 @@ package updatetelegrampost
 import (
 	"context"
 	"trip2g/internal/jobs"
-	"trip2g/internal/logger"
 	"trip2g/internal/model"
 )
 
 const JobID = "update_message"
 const QueueID = model.BackgroundTelegramAPICallQueue
 const Priority = 0
-
-type UpdateTelegramPostEnv interface {
-	Env
-	Logger() logger.Logger
-	RegisterJob(qID model.BackgroundQueueID, id string, handler func(ctx context.Context, m []byte) error)
-	EnqueueJob(ctx context.Context, job model.BackgroundTask) error
-}
 
 type UpdateTelegramPostJob struct {
 	enqueue jobs.EnqueueFunc
