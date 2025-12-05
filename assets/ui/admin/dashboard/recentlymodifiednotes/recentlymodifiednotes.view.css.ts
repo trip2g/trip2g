3f169dd7aae77787ce8e89281dd2eac780157844
Alexey Yurchenko <alexes.dev@gmail.com>
2025-12-05 15:38:52 +0000

    feat(assets/ui/admin/dashboard): add recentlymodifiednotes

---
+++ assets/ui/admin/dashboard/recentlymodifiednotes/recentlymodifiednotes.view.css.ts
@@ -0,0 +1,16 @@
+namespace $ {
+	const {rem} = $mol_style_unit
+
+	$mol_style_define($trip2g_admin_dashboard_recentlymodifiednotes, {
+		Title_labeler: {
+			flex: {
+				basis: rem(20),
+			}
+		},
+		Permalink_labeler: {
+			flex: {
+				basis: rem(20),
+			}
+		}
+	})
+}
