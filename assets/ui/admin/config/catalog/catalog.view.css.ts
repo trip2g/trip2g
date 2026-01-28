3b3b3333dbf4a8a50b1d437990fe624687a21ee3
Alexey Yurchenko <alexes.dev@gmail.com>
2026-01-28 06:05:05 +0000

    style(config): fix column widths in catalog

---
+++ assets/ui/admin/config/catalog/catalog.view.css.ts
@@ -0,0 +1,31 @@
+namespace $.$$ {
+	const { rem } = $mol_style_unit
+
+	$mol_style_define($trip2g_admin_config_catalog, {
+		Rows: {
+			flex: {
+				grow: 1,
+			},
+		},
+		Row_id_labeler: {
+			flex: {
+				basis: rem(15),
+			},
+		},
+		Row_value_labeler: {
+			flex: {
+				basis: rem(25),
+			},
+		},
+		UpdatedAt: {
+			flex: {
+				basis: rem(10),
+			},
+		},
+		UpdatedBy: {
+			flex: {
+				basis: rem(15),
+			},
+		},
+	})
+}
