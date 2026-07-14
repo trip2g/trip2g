8e866cb7c8d20d1e2ca683d53c1e95e4c74ae3a8
Alexey Yurchenko <alexes.dev@gmail.com>
2026-07-14 09:26:54 +0000

    fix(admin): wire SignOut button to inherited signout (drop shadowing null stub) (#235)

--- assets/ui/admin/admin.view.ts
+++ assets/ui/admin/admin.view.ts
@@ -1,5 +1,8 @@
 namespace $.$$ {
 	export class $trip2g_admin extends $.$trip2g_admin {
+		override signout() {
+			super.signout()
+		}
 	}
 
 	export class $trip2g_admin_app extends $.$trip2g_admin_app {
