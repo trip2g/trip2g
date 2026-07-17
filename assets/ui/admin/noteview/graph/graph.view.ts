a600516ebc62c9778e1215b0a694b02274ca76bf
Alexey Yurchenko <alexes.dev@gmail.com>
2026-07-17 15:18:36 +0000

    revert(assets/ui): exclude note-graph from gql codegen migration (follow-up after main merge)

--- assets/ui/admin/noteview/graph/graph.view.ts
+++ assets/ui/admin/noteview/graph/graph.view.ts
@@ -1,4 +1,52 @@
 namespace $.$$ {
+	const request = $trip2g_graphql_request(/* GraphQL */ `
+		query AdminGraph {
+			admin {
+				allSubgraphs {
+					nodes {
+						name
+						color
+					}
+				}
+				allLatestNoteViews {
+					nodes {
+						id
+						subgraphNames
+						title
+						pathId
+						free
+						isHomePage
+						graphPosition{
+							x,
+							y,
+						}
+						inLinks {
+							title
+							pathId
+							id
+						}
+					}
+				}
+			}
+		}
+`)
+
+	const mutate = $trip2g_graphql_request(/* GraphQL */ `
+		mutation UpdateNoteGraphPositions($input: UpdateNoteGraphPositionsInput!) {
+			admin {
+				payload: updateNoteGraphPositions(input: $input) {
+					__typename
+					... on UpdateNoteGraphPositionsPayload {
+						success
+					}
+					... on ErrorPayload {
+						message
+					}
+				}
+			}
+		}
+	`)
+
 	type graph_layout_node = {
 		id: string
 		subgraphNames: readonly string[]
@@ -222,7 +270,7 @@ namespace $.$$ {
 
 		@$mol_mem
 		data() {
-			const res = $trip2g_admin_noteview_graph_data()
+			const res = request()
 
 			return {
 				nodes: res.admin.allLatestNoteViews.nodes,
@@ -433,7 +481,7 @@ namespace $.$$ {
 
 			if( !positions.length ) return
 
-			const res = $trip2g_admin_noteview_graph_save({
+			const res = mutate({
 				input: { positions },
 			})
 
@@ -446,7 +494,7 @@ namespace $.$$ {
 		}
 
 		save_position( pathId: string, x: number, y: number ) {
-			const res = $trip2g_admin_noteview_graph_save({
+			const res = mutate({
 				input: {
 					positions: [{ pathId, x, y }]
 				},
